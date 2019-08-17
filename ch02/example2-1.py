c78b4abcb439f47939abb049e56fe8d114a5c30b
Victor Teodoro <victorjst@hotmail.com>
2019-08-17 13:37:15 -0300

    add: add more examples to the code
    I added more examples to the existing collection. Still going
    strong!

--- ch02/example2-1.py
+++ ch02/example2-1.py
@@ -4,3 +4,16 @@ codes = [ord(symbol) for symbol in symbols]
 codesAsString = ", ".join(str(code) for code in codes)
 
 print("The code points for the string '$¢£¥€¤' are", codesAsString)
+
+
+board = [['_'] * 3 for _ in range(3)]
+print("\nUsing a listcomp the original board is", board)
+
+board[0][2] = 'x'
+print("Now the board is", board)
+
+board = [['_'] * 3] * 3
+print("\nUsing multiplication the board is", board)
+
+board[0][2] = 'x'
+print("Now the board is", board)
