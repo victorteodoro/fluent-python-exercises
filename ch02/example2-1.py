25b5df4f7038fb6079d85c5dc15d6e97120680bd
Victor Teodoro <victorjst@hotmail.com>
2020-02-27 18:56:46 -0300

    Add more examples of the 2nd chapter

--- ch02/example2-1.py
+++ ch02/example2-1.py
@@ -1,3 +1,4 @@
+# Listcomps
 symbols = '$¢£¥€¤'
 
 codes = [ord(symbol) for symbol in symbols]
@@ -5,7 +6,13 @@ codesAsString = ", ".join(str(code) for code in codes)
 
 print("The code points for the string '$¢£¥€¤' are", codesAsString)
 
+# Tuples as records
+traveler_ids = [('USA', '31195855'), ('BRA', 'CE342567'), ('ESP', 'XDA205856')]
+for passport in sorted(traveler_ids):
+    print('%s/%s' % passport)
 
+
+# Using listcomps to nest lists
 board = [['_'] * 3 for _ in range(3)]
 print("\nUsing a listcomp the original board is", board)
 
